1ac04f61a76a2233d6c87999d67ebad3b711ac22
SxtyxmBhxrdwxj <111901770+Dialiser@users.noreply.github.com>
2024-12-01 13:34:48 +0530

    Update auth0-variables.js

--- src/auth0-variables.js
+++ src/auth0-variables.js
@@ -2,5 +2,5 @@ export const AUTH_CONFIG = {
   domain: "blog-samples.auth0.com",
   roleUrl: "https://rbac-tutorial-app/role",
   clientId: "tuX40w6IyxsGkLJNWKSAdf5J2D6eXe3I",
-  callbackUrl: "http://localhost:3000/satyam"
+  callbackUrl: "http://localhost:3000/callback"
 };
